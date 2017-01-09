5da589935e6571b31a2ff1747c4a0921883f6b86
Numkn644 <numkn644@gmail.com>
2017-01-09 22:26:12 +0900

    BezierCurveクラスを追加

---
+++ src/shooting/BezierCurve.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shooting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author Iinuma
+ */
+public class BezierCurve {
+    private List<Point> controlPoints = new ArrayList<>();
+    private List<Point> evaluatedPoints = new ArrayList<>();
+    private Integer grade;
+    
+    public BezierCurve(List<Point> controlPoints){
+        this.grade = controlPoints.size()-1;
+        this.controlPoints = controlPoints;
+    }
+    
+    public void evaluate(){
+        if(!this.evaluatedPoints.isEmpty()){
+            clearEvaluatedPoints();
+        }
+        
+        for(double t = 0; t < 1; t += 0.001){
+            evaluatedPoints.add(deCasteljau(t));
+        }
+    }
+    
+    private Point deCasteljau(double t){
+        List<List<Point>> EP = new ArrayList<>();
+        EP.add(this.controlPoints);
+        for(int i = 1; i < grade+1; i++){
+            List<Point> L = new ArrayList<>();
+            for(int j = 0; j < grade+1 - i; j++){
+                double x = (1-t)*EP.get(i-1).get(j).getX() + t *EP.get(i-1).get(j+1).getX();
+                double y = (1-t)*EP.get(i-1).get(j).getY() + t *EP.get(i-1).get(j+1).getY();
+
+                L.add(new Point(x,y));
+            }
+            EP.add(L);
+        }
+        return new Point(EP.get(grade).get(0).getX(), EP.get(grade).get(0).getY());
+    }
+    
+    public void clearEvaluatedPoints(){
+        evaluatedPoints.clear();
+    }
+    
+    public void setControlPoints(List<Point> newPoints){
+        this.controlPoints = newPoints;
+        this.grade = newPoints.size()-1;
+    }
+    
+    public List<Point> getEvaluatedPoints(){
+        return this.evaluatedPoints;
+    }
+}
--- src/shooting/Shooting.java
+++ src/shooting/Shooting.java
@@ -8,37 +8,76 @@ package shooting;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
  * @author Iinuma
  */
 public class Shooting extends Application {
+    private final Canvas canvas = new Canvas(640,480);
+    private List<Point> inputPoints = new ArrayList<>();
+    private BezierCurve bezier;
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+        
+        canvas.setOnMouseClicked(this::drawPoint);
+        Button clearBtn = new Button("Clear");
+        clearBtn.setOnMouseClicked((MouseEvent e) -> {
+            clearScreen();
+        });
+        Button lineBtn = new Button("Draw");
+        lineBtn.setOnMouseClicked((MouseEvent e)->{
+            for(int i = 0; i < inputPoints.size()-1;i++){
+                drawLine(inputPoints.get(i), inputPoints.get(i+1));
+            }
+        });
+        Button bezierBtn = new Button("Bezier");
+        bezierBtn.setOnMouseClicked((MouseEvent e)->{
+            bezier = new BezierCurve(inputPoints);
+            bezier.evaluate();
+            List<Point> ep = bezier.getEvaluatedPoints();
+            for(int i = 0; i < ep.size()-1; i++){
+                drawLine(ep.get(i), ep.get(i+1));
             }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Pane pane = new Pane(canvas);
+        pane.getChildren().add(clearBtn);
+        pane.getChildren().add(lineBtn);
+        pane.getChildren().add(bezierBtn);
+        Scene scene = new Scene(pane);
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        clearBtn.relocate(0, 0);
+        lineBtn.relocate(50, 0);
+        bezierBtn.relocate(100, 0);
+
+        primaryStage.setTitle("Shoot!!");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    public void drawPoint(MouseEvent e){
+        int r = 3;
+        canvas.getGraphicsContext2D().strokeOval(e.getX()-(r/2), e.getY()-(r/2), r, r);
+        inputPoints.add(new Point(e.getX(), e.getY()));
+    }
+    
+    public void drawLine(Point p1, Point p2){
+        canvas.getGraphicsContext2D().strokeLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+    }
+    
+    public void clearScreen(){
+        canvas.getGraphicsContext2D().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        inputPoints.clear();
     }
 
     /**
